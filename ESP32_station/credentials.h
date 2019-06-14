6e70d873e8e6408dc01f717636b62591458414cd
El loza <loza@usal.es>
2019-06-14 18:34:35 +0200

    First

---
+++ ESP32_station/credentials.h
@@ -0,0 +1,11 @@
+#ifndef CREDENTIALS_H_
+#define CREDENTIALS_H_
+/* WiFi username and password */
+const char* ssid = "";
+const char* password = "";
+const char* mqttServer = "broker.hivemq.com";
+const int mqttPort = 1883;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+
+#endif
---
+++ README.md
@@ -0,0 +1,2 @@
+# ESP32-BLE-MQTT-Arduino
+BLE Beacon Scanner to MQTT using MQTT
